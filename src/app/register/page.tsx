53e565c38ae7c3729f899c09f019533ebdb62e13
Mitiii <srp5628comust@gmail.com>
2025-04-23 15:47:10 +0900

    feat: 入力フォームを作成

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,6 +3,10 @@
 import { useState } from 'react'
 
 export default function RegistePage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         // ここにdbにユーザーを保存するapi
@@ -10,7 +14,9 @@ export default function RegistePage() {
 
     return (
         <form onSubmit={handleSubmit}>
-
+            <input type='text' value={name} onChange={e => setName(e.target.value)} placeholder='名前'/>
+            <input type='text' value={email} onChange={e=> setEmail(e.target.value)} placeholder='メールアドレス'/>
+            <input type='text' value={password} onChange={e=> setPassword(e.target.value)} placeholder='パスワード'/>
         </form>
     );
 }
