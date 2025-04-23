6edb512a036562860ec746180c03bd75f8f635f7
Mitiii <srp5628comust@gmail.com>
2025-04-24 00:08:54 +0900

    feat: apiの呼び出し処理の作成(途中まで)

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,16 +1,27 @@
 'use client'
 
 import { useState } from 'react'
+import axios from 'axios';
 
 export default function RegistePage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
         console.log('ボタンが押されたよー');
         e.preventDefault();
         // ここにdbにユーザーを保存するapi
+        try{
+            const response= await axios.post('/api/register', {
+                name,
+                email,
+                password
+            });
+            console.log('登録に成功しました',response.data);
+        }catch (error) {
+            console.log('登録に失敗しました',error);
+        }
     }
 
     return (
