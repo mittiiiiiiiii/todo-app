730b87c6c3b9d356d9843ec5d171e813f17db986
Mitiii <srp5628comust@gmail.com>
2025-04-24 01:23:25 +0900

    feat: ログインページを作成

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -25,11 +25,15 @@ export default function RegistePage() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type='text' value={name} onChange={e => setName(e.target.value)} placeholder='名前' required/>
-            <input type='text' value={email} onChange={e=> setEmail(e.target.value)} placeholder='メールアドレス' required/>
-            <input type='text' value={password} onChange={e=> setPassword(e.target.value)} placeholder='パスワード' required/>
-            <button type='submit'>登録</button>
-        </form>
+        <>
+            <h1>ユーザー登録ページ</h1>
+            <form onSubmit={handleSubmit}>
+                <input type='text' value={name} onChange={e => setName(e.target.value)} placeholder='名前' required/>
+                <input type='text' value={email} onChange={e=> setEmail(e.target.value)} placeholder='メールアドレス' required/>
+                <input type='text' value={password} onChange={e=> setPassword(e.target.value)} placeholder='パスワード' required/>
+                <button type='submit'>登録</button>
+            </form>
+            <p>ログインページは<a href='/login'>こちら</a></p>
+        </>
     );
 }
