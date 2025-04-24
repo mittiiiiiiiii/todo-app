9a6ab67771be2d6c80e5f52a8d220beb7b8f0fa9
Mitiii <srp5628comust@gmail.com>
2025-04-24 11:20:15 +0900

    feat: タスク追加ボタンを作成

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -4,6 +4,10 @@ export default function TasksPage() {
     return(
         <>
             <h1>タスク一覧ページ</h1>
+            <ul>
+                
+            </ul>
+            <button type="button">タスクを追加</button>
         </>
     );
 }
