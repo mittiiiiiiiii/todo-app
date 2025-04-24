54633d4f0e28a1590eaf93067ee024160ad7d42b
Mitiii <srp5628comust@gmail.com>
2025-04-24 11:24:51 +0900

    feat: 追加ボタンの処理を追加

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,13 +1,18 @@
 'use client'
 
 export default function TasksPage() {
+    const handleAddTask = () => {
+        console.log('タスクを追加するボタンが押されたよー');
+        // ここに追加ページに移動する処理を書く
+    }
+
     return(
         <>
             <h1>タスク一覧ページ</h1>
             <ul>
                 
             </ul>
-            <button type="button">タスクを追加</button>
+            <button type="button" onSubmit={handleAddTask}>タスクを追加</button>
         </>
     );
 }
