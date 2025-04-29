c00ce6597637c1d378b6546bd37a2bb9d319fa7e
Mitiii <srp5628comust@gmail.com>
2025-04-29 18:36:08 +0900

    feat: タイトル入力フォームを作成

--- src/app/tasks/new/page.tsx
+++ src/app/tasks/new/page.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function TasksnewPage(){
     const router = useRouter();
+    const [title, setTitle] = useState('');
 
     useEffect(() => {
         const checkLogin = async () => {
@@ -22,9 +25,21 @@ export default function TasksnewPage(){
         })();
     },[router]);
 
+    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+        console.log('タスクを追加するボタンが押されたよー');
+        e.preventDefault();
+        // ここにdbにタスクを保存するapi
+    }
+
     return (
         <>
             <h1>タスク追加ページ</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">タイトル（必須）</label>
+                    <input id="title" type='text' value={title} onChange={e => setTitle(e.target.value)} required/>
+                </div>
+            </form>
         </>
     );
 }
