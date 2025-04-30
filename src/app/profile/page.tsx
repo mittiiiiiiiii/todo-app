45d5ce5107b6ce2d32145ffbad01d1c6ef72f4db
Mitiii <srp5628comust@gmail.com>
2025-04-30 17:54:12 +0900

    feat: フォームを作成

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,6 +6,9 @@ import axios from 'axios'
 
 export default function ProfilePage() {
     const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
 
     useEffect(() => {
         const checkLogin = async () => {
@@ -18,15 +21,50 @@ export default function ProfilePage() {
             }
             return JSON.parse(user);
         };
+
+        const fetchUser = async (userId: number) => {
+            try{
+                const res = await axios.get('/api/profile', {params: { userId } });
+                console.log("ユーザーの取得に成功", res.data);
+            }catch(error){
+                console.log('ユーザー情報の取得に失敗しました',error);
+            }
+        }
        
         (async () => {
-            await checkLogin();
+            const user = await checkLogin();
+            await fetchUser(user.id);
         })();
     },[router]);
+
+    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+        console.log('ボタンが押されたよー');
+        e.preventDefault();
+        const user = JSON.parse(localStorage.getItem('user')||'{}');
+        try{
+            const response= await axios.post('/api/profile', {
+                userId: user.id,
+                name,
+                email,
+                password
+            });
+            console.log('編集に成功しました',response.data);
+            router.push('/tasks');
+        }catch (error) {
+            console.log('編集に失敗しました',error);
+        }
+    }
 
     return(
         <>
             <h1>プロフィール編集</h1>
+            <form onSubmit={handleSubmit}>
+                <input type='text' value={name} onChange={e => setName(e.target.value)} placeholder='名前' required/>
+                <input type='text' value={email} onChange={e=> setEmail(e.target.value)} placeholder='メールアドレス' required/>
+                <input type='text' value={password} onChange={e=> setPassword(e.target.value)} placeholder='パスワード' required/>
+                <button type='submit'>登録</button>
+            </form>
+            <p>ログインページは<a href='/login'>こちら</a></p>
         </>
     );
 }
