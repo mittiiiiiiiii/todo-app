611e5180428eb7e2b0fcdb29719102718d0e7eea
Mitiii <srp5628comust@gmail.com>
2025-05-01 12:13:43 +0900

    feat: ユーザーを編集するapiを作成

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -22,3 +22,25 @@ export async function GET(request: Request) {
         }, { status: 400 })
     }
 }
+
+export async function POST(request: Request) {
+    console.log('POSTリクエストを受け取りました');
+    const { userId, name, email, password } = await request.json();
+    try {
+        const user = await prisma.user.update({
+            where: { id:Number(userId) },
+            data: {
+                name,
+                email,
+                password
+            }
+        })
+        return NextResponse.json({ user },{ status: 200 })
+    }catch (error) {
+        console.log('登録エラー:', error);
+        return NextResponse.json({
+            message:  "登録に失敗しました",
+            error: error
+        }, { status: 400 })
+    }
+}
